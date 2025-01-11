11aeda545117046a5aaffe4e97f930d2d42101f9
tearshred <nick@MacBookPro.attlocal.net>
2025-01-11 17:03:15 -0500

    blah

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     productionBrowserSourceMaps: false,
+    experimental: {
+        optimizeFonts: true,
+      },
+      webpack: (config) => {
+        config.resolve.fallback = {
+          ...config.resolve.fallback,
+          fs: false,
+        };
+        return config;
+      },
 }
 
 module.exports = nextConfig
