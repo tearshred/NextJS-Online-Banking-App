9c7e2538445f2a704a77a5c64603f9ee370cd5c8
tearshred <nick@MacBookPro.attlocal.net>
2025-01-11 16:05:18 -0500

    updated reset password link

--- components/email-templates/password-reset.tsx
+++ components/email-templates/password-reset.tsx
@@ -5,15 +5,16 @@ import React from 'react';
 interface PasswordResetEmailTemplateProps {
   email: string;
   resetPasswordToken: string;
+  resetLink: string;
 }
 
 export const PasswordResetEmailTemplate: React.FC<Readonly<PasswordResetEmailTemplateProps>> = ({
-  email, resetPasswordToken
+  email, resetPasswordToken, resetLink
 }) => (
   <div>
     <h1>Reset Password for <b>{email}</b></h1>
     <p>Please follow the link below to reset your password. Link will expire after 24 hours</p>
-    <a href={`http://localhost:3000/auth/reset-password?token=${resetPasswordToken}`}>
+    <a href={`${resetLink}/auth/reset-password?token=${resetPasswordToken}`}>
       Click here to reset pasword
     </a>
   </div>
